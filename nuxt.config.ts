047bd057c750cdb95d248151e81c49f5f93b5817
zjjaxx <2495893564@qq.com>
2025-02-23 19:16:01 +0800

    production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      baseURL: process.env.BASE_URL
+      baseURL: process.env.BASE_URL??'http://47.92.125.144:3001'
     }
   },
   modules: ['@vant/nuxt'],
